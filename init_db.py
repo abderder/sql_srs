21eb862ec5ddb527828a911f7d3588f3e9b8194a
abderder <abderrazak.safi@gmail.com>
2024-10-21 17:18:29 +0200

    ajout d'une database avec les exercices

---
+++ init_db.py
@@ -0,0 +1,55 @@
+# pylint: disable=(missing-module-docstring)
+
+from io import StringIO
+
+import duckdb
+import pandas as pd
+
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
+# -------------------------------------
+# LISTES EXERCICES
+# -------------------------------------
+data = {
+    "theme": ["cross_join","window_functions"],
+    "exercises_name":["food_and_price","electronic_sales"],
+    "tables":[["food","price"],"sales"],
+    "last_reviewed":["1970-01-01","1970-01-01"]
+}
+memory_state_df = pd.DataFrame(data)
+
+DATA_FOOD = """food_id,food_name
+1,Pizza
+2,Burger
+3,Sushi
+4,Pasta
+5,Salad
+"""
+food = pd.read_csv(StringIO(DATA_FOOD))
+con.execute("CREATE TABLE IF NOT EXISTS food as SELECT * FROM food")
+
+# Données Price en format CSV
+DATA_PRICE = """food_id,price
+1,8.99
+2,5.49
+3,12.99
+4,7.50
+6,4.99
+"""
+price = pd.read_csv(StringIO(DATA_PRICE))
+con.execute("CREATE TABLE IF NOT EXISTS price as SELECT * FROM price")
+
+DATA_SALES = """
+id,product,category,quantity,price,sales_date
+1,Keyboard,Electronics,10,25.50,2024-10-01
+2,Mouse,Electronics,20,15.00,2024-10-01
+3,Laptop,Electronics,5,1200.00,2024-10-02
+4,Chair,Furniture,12,85.00,2024-10-02
+5,Desk,Furniture,7,300.00,2024-10-03
+6,Headphones,Electronics,15,99.99,2024-10-03
+7,Lamp,Furniture,18,45.00,2024-10-04
+8,Monitor,Electronics,3,250.00,2024-10-05
+9,Table,Furniture,9,400.00,2024-10-05
+10,Smartphone,Electronics,25,699.99,2024-10-06
+"""
+sales = pd.read_csv(StringIO(DATA_SALES))
--- script.py
+++ script.py
@@ -1,30 +1,11 @@
 # pylint: disable=(missing-module-docstring)
 
-from io import StringIO
 
-import duckdb
-import pandas as pd
 import streamlit as st
 
-DATA_FOOD = """food_id,food_name
-1,Pizza
-2,Burger
-3,Sushi
-4,Pasta
-5,Salad
-"""
+import duckdb
 
-# Données Price en format CSV
-DATA_PRICE = """food_id,price
-1,8.99
-2,5.49
-3,12.99
-4,7.50
-6,4.99
-"""
-
-food = pd.read_csv(StringIO(DATA_FOOD))
-price = pd.read_csv(StringIO(DATA_PRICE))
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 st.title(
     """
@@ -32,60 +13,61 @@ SQL SRS
 Spaced Repetition System SQL parctice
 """
 )
-SQL_RESULT = """
-SELECT f.food_id, f.food_name, p.price
-FROM food f
-INNER JOIN price p ON f.food_id = p.food_id
 
-"""
-data_result = duckdb.query(SQL_RESULT).df()
-with st.sidebar:
-    option = st.selectbox(
-        "what would you like to revise?",
-        ["JOIN", "GROUP BY", "WINDOWS FUNCTIONS"],
-        index=0,
-        placeholder="Select a theme...",
-    )
-    st.write("You selected:", option)
-
-st.markdown(
-    "Récupère le nom des plats et leur prix en utilisant "
-    "une jointure entre les tables `food` et `price`"
-)
-
-sql_query = st.text_area(label="Entrez votre requete: ")
-try:
-    sql_table = duckdb.query(sql_query).df()
-    st.dataframe(sql_table)
-    sql_table_sorted = (
-        sql_table.sort_index(axis=1)
-        .sort_values(by=list(sql_table.columns))
-        .reset_index(drop=True)
-    )
-    data_result_sorted = (
-        data_result.sort_index(axis=1)
-        .sort_values(by=list(data_result.columns))
-        .reset_index(drop=True)
-    )
-    if sql_table_sorted.equals(data_result_sorted):
-        st.success("Correct ! La réponse est bien.")
-    else:
-        st.error("C'est Faux.")
-
-except AttributeError as e:
-    st.warning("Please enter a SQL query.")
-
-
-tab1, tab2 = st.tabs(["Tables", "Solution"])
-
-with tab1:
-    st.write("table: food")
-    st.dataframe(food)
-    st.write("table: price")
-    st.dataframe(price)
-    st.write("table: expected")
-    st.dataframe(data_result)
-with tab2:
-    st.write("Réponse:")
-    st.code(SQL_RESULT, language="sql")
-    st.dataframe(data_result)
+# SQL_RESULT = """
+# SELECT f.food_id, f.food_name, p.price
+# FROM food f
+# INNER JOIN price p ON f.food_id = p.food_id
+#
+# """
+# data_result = duckdb.query(SQL_RESULT).df()
+# with st.sidebar:
+#     option = st.selectbox(
+#         "what would you like to revise?",
+#         ["JOIN", "GROUP BY", "WINDOWS FUNCTIONS"],
+#         index=0,
+#         placeholder="Select a theme...",
+#     )
+#     st.write("You selected:", option)
+#
+# st.markdown(
+#     "Récupère le nom des plats et leur prix en utilisant "
+#     "une jointure entre les tables `food` et `price`"
+# )
+#
+# sql_query = st.text_area(label="Entrez votre requete: ")
+# try:
+#     sql_table = duckdb.query(sql_query).df()
+#     st.dataframe(sql_table)
+#     sql_table_sorted = (
+#         sql_table.sort_index(axis=1)
+#         .sort_values(by=list(sql_table.columns))
+#         .reset_index(drop=True)
+#     )
+#     data_result_sorted = (
+#         data_result.sort_index(axis=1)
+#         .sort_values(by=list(data_result.columns))
+#         .reset_index(drop=True)
+#     )
+#     if sql_table_sorted.equals(data_result_sorted):
+#         st.success("Correct ! La réponse est bien.")
+#     else:
+#         st.error("C'est Faux.")
+#
+# except AttributeError as e:
+#     st.warning("Please enter a SQL query.")
+#
+#
+# tab1, tab2 = st.tabs(["Tables", "Solution"])
+#
+# with tab1:
+#     st.write("table: food")
+#     st.dataframe(food)
+#     st.write("table: price")
+#     st.dataframe(price)
+#     st.write("table: expected")
+#     st.dataframe(data_result)
+# with tab2:
+#     st.write("Réponse:")
+#     st.code(SQL_RESULT, language="sql")
+#     st.dataframe(data_result)
